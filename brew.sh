7a4b7ad6097920754f639715303d39e33aaaadfe
Troy McCall <troyxmccall@gmail.com>
2015-03-05 14:45:19 -0500

    config git

--- brew.sh
+++ brew.sh
@@ -105,6 +105,17 @@ brewn install ruby-build
 #install git
 brew install git
 
+#config git
+curl -s -O \
+https://github-media-downloads.s3.amazonaws.com/osx/git-credential-osxkeychain
+
+chmod u+x git-credential-osxkeychain
+
+sudo mv git-credential-osxkeychain \
+"$(dirname $(which git))/git-credential-osxkeychain"
+
+git config --global credential.helper osxkeychain
+
 # Install io.js
 brew install iojs
 
