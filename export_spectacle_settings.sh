84a326bb0e7d72c97c4b89d29cb764e1ed23db20
Troy McCall <troyxmccall@gmail.com>
2015-04-14 14:02:36 -0400

    no glasses settings

--- export_spectacle_settings.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for key in MakeLarger MakeSmaller MoveToBottomDisplay MoveToBottomHalf MoveToCenter MoveToFullscreen MoveToLeftDisplay MoveToLeftHalf MoveToLowerLeft MoveToLowerRight MoveToNextDisplay MoveToNextThird MoveToPreviousDisplay MoveToPreviousThird MoveToRightDisplay MoveToRightHalf MoveToTopDisplay MoveToTopHalf MoveToUpperLeft MoveToUpperRight RedoLastMove UndoLastMove; do
-  printf "defaults write com.divisiblebyzero.Spectacle ${key} -data ";
-  defaults read com.divisiblebyzero.Spectacle MoveToCenter | sed 's/[^0-9a-f]//g';
-done
