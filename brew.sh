2a826f0a49ba27117def0cb6b3158b0ede29f7e0
Troy McCall <troyxmccall@gmail.com>
2015-05-18 14:33:59 -0400

    show battery and init ssh keys

--- brew.sh
+++ brew.sh
@@ -137,6 +137,15 @@ sudo mv git-credential-osxkeychain \
 
 git config --global credential.helper osxkeychain
 
+#dat private repo access
+pub=$HOME/.ssh/id_rsa.pub
+echo 'Checking for SSH key, generating one if it does not exist...'
+  [[ -f $pub ]] || ssh-keygen -t rsa
+
+echo 'Copying public key to clipboard. Paste it into your Github account...'
+  [[ -f $pub ]] && cat $pub | pbcopy
+  open 'https://github.com/account/ssh'
+
 # Install io.js
 brew install iojs
 
