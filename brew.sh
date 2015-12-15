7d7dc66eae0fe77aa4d15658dc5959d9a46c4486
Troy McCall <troyxmccall@gmail.com>
2015-12-15 14:50:15 -0500

    default global node modules

--- brew.sh
+++ brew.sh
@@ -145,7 +145,29 @@ brew install zopfli
 brew install node
 # install phantomjs for el capitan
 npm install phantom phantomjs -g
-# yosemite | brew install phantomjs
+# front-end web, client and tasks modules
+npm install -g bower
+npm install -g grunt-cli grunt
+npm install -g gulp
+npm install -g lodash
+npm install -g forever
+npm install -g browserify
+# frameworks
+npm install -g socket.io
+npm install -g express
+npm install -g mocha
+#libraries
+npm install -g underscore
+npm install -g moment
+npm install -g q
+npm install -g passport
+#packages
+npm install -g coffee-script
+# hubot
+npm install -g yo generator-hubot
+# other utilities
+npm install -g debug
+npm install -g jshint
 
 #install rbenv and ruby build
 brew install rbenv
