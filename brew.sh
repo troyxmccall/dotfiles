dbf1f4df8fff18e8c9cf5eeaf2bcdd75f26ba10c
Troy McCall <troyxmccall@gmail.com>
2016-05-16 16:09:29 -0400

    use brew installed bash

--- brew.sh
+++ brew.sh
@@ -45,7 +45,13 @@ brew install gnu-sed --with-default-names
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
-brew install bash-completion
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
