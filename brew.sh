6242c71d187bc7d74b4ed7b2a65436f1c670f992
Troy McCall <troyxmccall@gmail.com>
2019-12-09 15:50:32 -0500

    change bashcompletion

--- brew.sh
+++ brew.sh
@@ -56,7 +56,7 @@ brew install gnu-sed --with-default-names
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
-brew install bash-completion2
+brew install bash-completion
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
