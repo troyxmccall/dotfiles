7ccf45f78b1129a23a3719abd4cda68f79dfcac9
Troy McCall <troyxmccall@gmail.com>
2020-03-15 14:41:59 -0400

    some node update

--- npm.sh
+++ npm.sh
@@ -5,35 +5,36 @@ nvm_versions=(
   lts/boron
   lts/carbon
   lts/dubnium
+  lts/erbium
 )
 
 echo "installing stable, boron, and carbon lts node versions and all global packages to 'em. this may take a few minutes"
 
 for nvm_version in "${nvm_versions[@]}"
 do
-  nvm install $nvm_version
+  nvm install $nvm_version --reinstall-packages-from=node
 
   # front-end web, client and tasks modules
   npm install -g bower
   npm install -g grunt-cli grunt
   npm install -g gulp-cli gulp
-  npm install -g lodash
-  npm install -g forever
-  npm install -g browserify
+  # npm install -g lodash
+  # npm install -g forever
+  # npm install -g browserify
   # frameworks
-  npm install -g socket.io
-  npm install -g express
-  npm install -g mocha
-  npm install -g angular
-  #libraries
-  npm install -g underscore
-  npm install -g moment
-  npm install -g q
-  npm install -g passport
+  # npm install -g socket.io
+  # npm install -g express
+  # npm install -g mocha
+  # npm install -g angular
+  # #libraries
+  # npm install -g underscore
+  # npm install -g moment
+  # npm install -g q
+  # npm install -g passport
   #packages
   npm install -g coffee-script
   # hubot
-  npm install -g yo generator-hubot
+  # npm install -g yo generator-hubot
   # other utilities
   npm install -g debug
   npm install -g jshint
