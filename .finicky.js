163079f1b7de52124eb992134d5d697959fa8e78
Troy McCall <troyxmccall@gmail.com>
2020-04-24 15:55:05 -0400

    update behavior

--- .finicky.js
+++ .finicky.js
@@ -2,14 +2,14 @@ module.exports = {
   defaultBrowser: "Firefox Developer Edition",
   handlers: [{
       // Open apple.com urls in Safari
-      match: finicky.matchDomains(["apple.com"]),
+      match: finicky.matchHostnames(["apple.com"]),
       browser: "Safari"
   }, {
-      match: finicky.matchDomains(["google.com"]),
+      match: finicky.matchHostnames(["google.com", /.*\.google.com$/, "basecamp.com", /.*\.basecamp.com$/]),
       browser: "Google Chrome"
   },
     {
-      match: finicky.matchDomains(["reddit.com", "twitter.com", "instagram.com", "tumblr.com", "amazon.com"]),
+      match: finicky.matchHostnames(["reddit.com", "twitter.com", "instagram.com", "tumblr.com", "amazon.com"]),
       browser: "Brave Browser"
   }
   ]
