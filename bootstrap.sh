eeae218a42b7808a7b64269be10e226fe7fc7188
Troy McCall <troyxmccall@gmail.com>
2021-05-13 16:39:14 -0400

    handle submodules

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
+  git submodule update --init --recursive;
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
     --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
   source ~/.bash_profile;
